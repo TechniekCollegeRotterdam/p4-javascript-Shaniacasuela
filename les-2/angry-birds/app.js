d1ccceccac69ad40ae3310eef3833fbb7303a563
Shaniacasuela <9011037@student.zadkine.nl>
2022-06-03 12:57:53 +0200

    oefening 3 - Change the moving bird

--- les-2/angry-birds/app.js
+++ les-2/angry-birds/app.js
@@ -18,20 +18,25 @@ window.addEventListener("keydown",function(e){
     if(e.key == "ArrowRight"){
         px= px + 50;
         bird.style.left = px + "px";
+        bird.src = "img/b-right.svg";
     }
     if(e.key == "ArrowLeft"){
         px= px - 50;
         bird.style.left = px + "px";
+        bird.src = "img/b-left.svg";
     }
 
     if(e.key == "ArrowUp"){
         px= px + 50;
         bird.style.bottom = px + "px";
+        bird.src = "img/b-up.svg";
     }
 
     if(e.key == "ArrowDown"){
         px= px - 50;
         bird.style.bottom = px + "px";
+        bird.src = "img/b-down.svg";
     }
 
 })
+
